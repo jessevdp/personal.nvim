69b04b4bac4277c440425db3c245b94aaa30c82c
Jesse van der Pluijm <jessevdp@hey.com>
2024-07-02 17:30:10 +0200

    Initialize basic NeoVim config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- Set <space> as the leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>.
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Disable arrow keys in normal mode
+vim.keymap.set("n", "<left>", "<cmd>echo 'Use h to move!!'<CR>")
+vim.keymap.set("n", "<right>", "<cmd>echo 'Use l to move!!'<CR>")
+vim.keymap.set("n", "<up>", "<cmd>echo 'Use k to move!!'<CR>")
+vim.keymap.set("n", "<down>", "<cmd>echo 'Use j to move!!'<CR>")
---
+++ lua/config/options.lua
@@ -0,0 +1,50 @@
+-- Enable mouse mode
+vim.opt.mouse = "a"
+
+-- Make line numbers default
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- NOTE: this shouldn't be needed since we got vim-sleuth.. perhaps remove?
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- Wrapped lines are indented to match
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default (for consistent layout)
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 600
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
