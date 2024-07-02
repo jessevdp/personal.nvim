74947339117fc08b28ebf17bf5bd8e1dfe795ae8
Jesse van der Pluijm <jessevdp@hey.com>
2024-07-02 17:44:37 +0200

    Setup neo-tree plugin via Lazy

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window
+    },
+    cmd = "Neotree",
+    keys = {
+      { "\\", ":Neotree reveal<CR>", { desc = "NeoTree reveal" } },
+    },
+    opts = {
+      window = {
+        position = "right",
+        mappings = {
+          ["\\"] = "close_window",
+        },
+      },
+    },
+  },
+  {
+    -- allows picking a window to open the file into using "w" in neo-tree
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    opts = {
+      hint = "floating-big-letter",
+    },
+  }
+}
+
