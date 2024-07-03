8320c8334fafbd43e900ad76a8ceb925b8da8c3f
Jesse van der Pluijm <jessevdp@hey.com>
2024-07-03 09:20:00 +0200

    Disable rulers for plugin specific buffers to reduce noise

--- lua/plugins/smartcolumn.lua
+++ lua/plugins/smartcolumn.lua
@@ -1,7 +1,18 @@
 return {
-  "m4xshen/smartcolumn.nvim",
+  "m4xshen/smartcolumn.nvim", -- rulers
   opts = {
-    colorcolumn = { 81, 121 }
+    colorcolumn = { 81, 121 },
+    disabled_filetypes = {
+      "help",
+      "text",
+      "markdown",
+
+      -- disable rulers for plugin specific buffers
+      "lazy",
+      "checkhealth",
+      "alpha",
+      "neo-tree",
+    },
   },
 }
 
