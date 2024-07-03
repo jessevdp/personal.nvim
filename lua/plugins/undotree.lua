ff439bece0641605349bafb11d9539e18ae1256b
Jesse van der Pluijm <jessevdp@hey.com>
2024-07-03 11:47:57 +0200

    Use regular (non pop-up) window for undotree diff

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -2,6 +2,7 @@ return {
   "jiaoshijie/undotree",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
+    float_diff = false,
   },
   init = function()
     vim.keymap.set("n", "<leader>u", require("undotree").toggle, { desc = "Toggle [U]ndotree" })
