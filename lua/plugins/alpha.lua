af201711d5e173a81ac3af70b1c22f8285553360
Jesse van der Pluijm <jessevdp@hey.com>
2024-07-04 09:59:01 +0200

    Add useful buttons to Alpha start-screen

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -23,8 +23,15 @@ return {
       [[                                                                       ]],
     }
 
-    -- disable MRU
+    -- disable global MRU: I only want project specific files to show
     dashboard.section.mru.val = { { type = "padding", val = 0 } }
+
+    dashboard.section.bottom_buttons.val = {
+      dashboard.button("f", "Fuzzy-find file" , ":Telescope find_files<CR>"),
+      dashboard.button("\\", "File tree" , ":Neotree reveal<CR>"),
+      dashboard.button("g", "Get good" , ":VimBeGood<CR>"),
+      dashboard.button("q", "Quit" , ":qa<CR>"),
+    }
 
     dashboard.section.footer.val = {
       {
