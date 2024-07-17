ef5fb22f6ec9a47c744640e17703501de58e3c47
Jesse van der Pluijm <jessevdp@hey.com>
2024-07-17 08:54:19 +0200

    Setup keybinds to move lines in visual mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,3 +15,8 @@ vim.keymap.set("n", "<left>", "<cmd>echo 'Use h to move!!'<CR>")
 vim.keymap.set("n", "<right>", "<cmd>echo 'Use l to move!!'<CR>")
 vim.keymap.set("n", "<up>", "<cmd>echo 'Use k to move!!'<CR>")
 vim.keymap.set("n", "<down>", "<cmd>echo 'Use j to move!!'<CR>")
+
+-- Use J and K to move lines in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
