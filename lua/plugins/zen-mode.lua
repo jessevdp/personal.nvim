7b2d29bcca1aa439b662aa318fb280ecbe4f70c5
Jesse van der Pluijm <jessevdp@hey.com>
2024-07-30 16:37:46 +0200

    Setup zen-mode & twilight plugins via Lazy

---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      window = {
+        backdrop = 1,
+      },
+      plugins = {
+        twilight = { enabled = true },
+      },
+    },
+  },
+  {
+    "folke/twilight.nvim",
+    opts = {},
+  }
+}
+
