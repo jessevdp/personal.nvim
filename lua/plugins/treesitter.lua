f5e2e4be81dd59dc5c2a27e037c6546cf9bbdb47
Jesse van der Pluijm <jessevdp@hey.com>
2024-08-04 11:53:35 +0200

    Fix typo in treestitter config to auto-enable highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     ensure_installed = { "bash", "diff", "lua", "luadoc", "markdown", "vim", "vimdoc" },
     auto_install = true,
     highlight = {
-      enabled = true,
+      enable = true,
       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
       --  If you are experiencing weird indenting issues, add the language to
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
