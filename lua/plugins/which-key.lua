c4a25e2610c4fbdebe04702d43c918f498e47187
Jesse van der Pluijm <jessevdp@hey.com>
2024-08-25 13:28:36 +0200

    Document all <leader> keymaps in which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,7 +1,7 @@
 return {
   -- Show pending keybinds
   "folke/which-key.nvim",
-  event = 'VimEnter',
+  event = "VimEnter",
   init = function()
     -- Set mapped sequence wait time
     -- Determines when which-key popup shows
@@ -10,9 +10,22 @@ return {
   end,
   config = function()
     -- Document existing key chains
-    require('which-key').register {
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+    require("which-key").register {
+      ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+      ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+      ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
+      ["<leader>gh"] = { name = "[G]it [H]unk", _ = "which_key_ignore" },
+      ["<leader>h"] = { name = "[H]arpoon", _ = "which_key_ignore" },
+      ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+      ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+      ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
     }
+    -- visual mode
+    require("which-key").register({
+      ["<leader>g"] = { "[G]it" },
+      ["<leader>gh"] = { "Git [H]unk" },
+      ["<leader>s"] = { "[S]earch" },
+    }, { mode = "v" })
   end,
 }
 
