3219f824283ccefa8c0c911edd2c811952eb452d
Jesse van der Pluijm <jessevdp@hey.com>
2024-10-17 08:34:08 +0200

    fixup! Add CopyRelativePath user command

--- lua/config/usercmds/copy-relative-path.lua
+++ lua/config/usercmds/copy-relative-path.lua
@@ -1,4 +1,6 @@
 vim.api.nvim_create_user_command("CopyRelativePath", function()
+  vim.fn.chdir(".")
+
   local path = vim.fn.expand("%")
   vim.fn.setreg("+", path)
   vim.notify('Copied "' .. path .. '" to the clipboard!')
