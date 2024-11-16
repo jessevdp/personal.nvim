de160db6224527b748dea0178da3b039b84946fc
Jesse van der Pluijm <jessevdp@hey.com>
2024-11-16 09:15:36 +0100

    Add keymap to copy last yank to system clipboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -45,4 +45,5 @@ vim.keymap.set("n", "]Q", "<cmd>clast<CR>zz", { desc = "Go to last [Q]uickfix lo
 -- Shortcut to copy into system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>+", "<cmd>let @+=@0<CR>", { desc = "Copy last yank to system clipboard" })
 
