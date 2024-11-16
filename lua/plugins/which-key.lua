7fc48d453c2ac7078e469831b15a47504d4fae4f
Jesse van der Pluijm <jessevdp@hey.com>
2024-11-16 13:41:33 +0100

    Setup which-key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    -- Set mapped sequence wait time
+    -- Determines when which-key popup shows
+    vim.opt.timeout = true
+    vim.opt.timeoutlen = 900
+  end,
+  opts = {
+    spec = {
+      { "<leader>s", group = "[S]earch" },
+    },
+  },
+  keys = {
+    { "<leader>?", function() require("which-key").show({ global = true }) end, desc = "Buffer Keymaps" },
+  },
+}
