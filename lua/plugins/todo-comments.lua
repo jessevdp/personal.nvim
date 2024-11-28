932dd071b2c2a177248668e3e1e06e7a81fae373
Jesse van der Pluijm <jessevdp@hey.com>
2024-11-28 07:48:41 +0100

    Only highlight the TODO comment keyword, not the space around it
    It was bugging me that this highlight was touching the comment character
    (ex: `#`). Just highlighting the word itself seems much cleaner!
    
    ```rb
    # TODO: do all the things
    # BUG: fix me please
    ```

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -3,6 +3,10 @@ return {
     "folke/todo-comments.nvim",
     event = { "BufReadPost", "BufNewFile", "VeryLazy" },
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {},
+    opts = {
+      highlight = {
+        keyword = "bg",
+      },
+    },
   },
 }
