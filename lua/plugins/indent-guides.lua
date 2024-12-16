5064059b144bcac0605bcd07f651a51b22fb3905
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-16 10:01:17 +0100

    Hide first (non-indented) indentation guide

--- lua/plugins/indent-guides.lua
+++ lua/plugins/indent-guides.lua
@@ -12,5 +12,9 @@ return {
         show_end = false,
       },
     },
+    init = function()
+      local hooks = require "ibl.hooks"
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+    end
   },
 }
