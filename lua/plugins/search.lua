369cdf888ec7ea37590c95d125d41c854def340b
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-19 19:00:58 +0100

    Remap "search recent files" to "<leader>sr"

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -11,9 +11,9 @@ return {
     { "<leader>sg", "<cmd>Telescope live_grep<CR>", desc = "[S]earch by [G]rep" },
     { "<leader>sw", "<cmd>Telescope grep_string<CR>", desc = "[S]earch current [W]ord" },
     { "<leader>sh", "<cmd>Telescope help_tags<CR>", desc = "[S]earch [H]elp" },
-    { "<leader>s.", "<cmd>Telescope oldfiles<CR>", desc = "[S]earch Recent Files ('.' for repeat)" },
+    { "<leader>sr", "<cmd>Telescope oldfiles<CR>", desc = "[S]earch [R]ecent Files" },
     { "<leader><leader>", "<cmd>Telescope buffers<CR>", desc = "Search Existing Buffers" },
-    { "<leader>sr", "<cmd>Telescope resume<CR>", desc = "[S]earch [R]esume" },
+    { "<leader>s.", "<cmd>Telescope resume<CR>", desc = "[S]earch Resume" },
     {
       "<leader>s/",
       function()
