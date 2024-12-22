deaa0071ef710fd3b44e1b01415d27a0d0093046
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-22 12:38:14 +0100

    Change up completion keymaps

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -7,11 +7,11 @@ return {
     ---@type blink.cmp.Config
     opts = {
       keymap = {
-        ["<C-e>"] = { "show", "hide" },
+        ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide" },
         ["<C-y>"] = { "select_and_accept" },
         ["<C-p>"] = { "select_prev", "fallback" },
         ["<C-n>"] = { "select_next", "fallback" },
-        ["<C-?>"] = { "show_documentation", "hide_documentation" },
         ["<C-b>"] = { "scroll_documentation_up", "fallback" },
         ["<C-f>"] = { "scroll_documentation_down", "fallback" },
         ["<Tab>"] = { "snippet_forward", "fallback" },
@@ -20,15 +20,15 @@ return {
       appearance = {
         nerd_font_variant = "mono",
       },
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-      },
       completion = {
         menu = {
           auto_show = false,
         },
       },
       signature = { enabled = true },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
     },
   },
 }
