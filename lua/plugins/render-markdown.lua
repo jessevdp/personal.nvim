05993d27dc45bf084113e6683ddad92e94d55308
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-30 12:26:24 +0100

    Fix lazy-loading of render-markdown for CodeCompanion

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,14 +1,13 @@
 return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    ft = { "markdown" },
+    ft = {
+      "markdown",
+      "codecompanion",
+    },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {
-      file_types = {
-        "markdown",
-        "codecompanion",
-      },
       -- Workaround for highlight issue related to breakindent.
       -- See: https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/149
       win_options = { breakindent = { default = true, rendered = false } },
