0ad132bf2d6dced42f2fa5d4a2bcb862e4e35461
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-30 13:11:42 +0100

    Style headings in rendered markdown

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -11,6 +11,10 @@ return {
       -- Workaround for highlight issue related to breakindent.
       -- See: https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/149
       win_options = { breakindent = { default = true, rendered = false } },
+      heading = {
+        width = "block",
+        right_pad = 1,
+      },
     },
   },
 }
