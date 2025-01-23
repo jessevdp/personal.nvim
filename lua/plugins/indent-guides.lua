304368a02a05c8965b7c6d23ab43f80bfc087547
Jesse van der Pluijm <jessevdp@hey.com>
2025-01-23 16:36:16 +0100

    Format code

--- lua/plugins/indent-guides.lua
+++ lua/plugins/indent-guides.lua
@@ -13,7 +13,7 @@ return {
       },
     },
     init = function()
-      local hooks = require "ibl.hooks"
+      local hooks = require("ibl.hooks")
       hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
     end
   },
