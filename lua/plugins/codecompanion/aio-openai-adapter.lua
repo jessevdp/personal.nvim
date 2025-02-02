1786c802235ccfb9bfc8967e7df33834dedda88d
Jesse van der Pluijm <jessevdp@hey.com>
2025-02-02 12:21:38 +0100

    Update CodeCompanion AiO OpenAI adapter to new API

--- lua/plugins/codecompanion/aio-openai-adapter.lua
+++ lua/plugins/codecompanion/aio-openai-adapter.lua
@@ -2,10 +2,10 @@ local M = {}
 
 local config = {
   name = "aio_openai",
+  formatted_name = "AiO OpenAI",
   env = {
-    url = "https://iogpt-api-management-service.azure-api.net/openai/api/proxy/openai",
-    api_key = os.getenv("IOGPT_API_KEY"),
-    chat_url = "/chat/completions",
+    api_key = "cmd:cat ~/.aio.key", -- TODO: move to secret store
+    endpoint = "https://aio.azure-api.net",
   },
   schema = {
     model = {
@@ -13,26 +13,14 @@ local config = {
       choices = {
         "gpt-4o",
         "gpt-4o-mini",
-        "gpt-4",
-        "gpt-4-32k",
-        "gpt-3.5-turbo",
-        "gpt-3.5-turbo-16k",
+        ["o1-preview"] = { opts = { stream = false } },
       },
     },
-  },
-  handlers = {
-    -- AiO OpenAI proxy does not accept `{ "options": {} }` in the POST body
-    form_parameters = function(self, params, messages)
-      local openai = require("codecompanion.adapters.openai")
-      local parameters = openai.handlers.form_parameters(self, params, messages)
-      parameters["options"] = nil
-      return parameters
-    end,
   },
 }
 
 M.make = function()
-  return require("codecompanion.adapters").extend("openai_compatible", config)
+  return require("codecompanion.adapters").extend("azure_openai", config)
 end
 
 return M
