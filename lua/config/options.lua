2a964b1c3353d9b5886f2032720ad0ad30e6da70
Jesse van der Pluijm <jessevdp@hey.com>
2025-03-19 14:09:58 +0100

    Make .jbuilder files behave like Ruby

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -71,3 +71,10 @@ vim.opt.shortmess:append({
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+-- filetypes
+vim.filetype.add({
+  extension = {
+    jbuilder = "ruby",
+  },
+})
